85506778718d7ac82813cafe7979c5c4f36422fa
chandans_webelight <chandans@webelight.co.in>
2022-11-08 11:12:34 +0530

    Added Blog App

---
+++ fastapi/blogapp/backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from core.config import settings
+from apis.general_pages.route_homepage import general_pages_router
+
+
+def include_router(app):
+	app.include_router(general_pages_router)
+
+
+def start_application():
+	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+	include_router(app)
+	return app 
+
+
+app = start_application()
